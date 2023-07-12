80e3d34c9cda3f56ec3b2649d65d1ecd5076d5e3
Wandiya James <jameswandiya@gmail.com>
2023-07-12 21:39:57 +0100

    added dropdown profile menu

--- scripts/admindashboard.js
+++ scripts/admindashboard.js
@@ -11,6 +11,52 @@ if (navMenu) {
     });
 }
 
+/*************************** Dropdown Content **********************/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownBtn = document.querySelector(".profile-btn");
+    const dropdownContent = document.querySelector(".dropdown-content");
+  
+    dropdownBtn.addEventListener("click", function() {
+      dropdownContent.style.display = dropdownContent.style.display === "none" ? "block" : "none";
+    });
+  
+    document.addEventListener("click", function(event) {
+      if (!event.target.closest(".dropdown")) {
+        dropdownContent.style.display = "none";
+      }
+    });
+  });
+  
+    document.addEventListener("DOMContentLoaded", function() {
+    const editLink = document.querySelector(".edit");
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+  
+    editLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      fileInput.click();
+    });
+  
+    fileInput.addEventListener("change", function() {
+      const file = fileInput.files[0];
+      const profileImage = document.querySelector(".profile-btn img");
+  
+      // Perform file upload logic here
+      // You can use AJAX or other methods to upload the file to the server
+  
+      // For demonstration purposes, update the profile image source with the selected file
+      const reader = new FileReader();
+      reader.onload = function() {
+        profileImage.src = reader.result;
+      };
+      reader.readAsDataURL(file);
+    });
+  });
+  
+
+
+  
 // document.addEventListener('click', function(event) {
 // const target = event.target;
 // if (target.classList.contains('menu-btn')) {
